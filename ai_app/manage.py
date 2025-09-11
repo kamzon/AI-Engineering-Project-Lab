4fd9b0801f79f77a24eec4953431fbddd761e21d
Haidar Chaito <chaito01@ads.uni-passau.de>
2025-09-12 01:37:04 +0200

    debugging ci pipeline

--- ai_app/manage.py
+++ ai_app/manage.py
@@ -3,7 +3,9 @@
 import os
 import sys
 
-# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+print("appending to sys.path")
+print(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 
 def main():
